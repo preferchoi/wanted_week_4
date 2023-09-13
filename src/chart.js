8081d43656c2b42cc25949cb656f9fa1f4ffa481
prefer_choi <prefer_choi@naver.com>
2023-09-14 02:45:21 +0900

    feat: chart 추가

--- src/chart.js
+++ src/chart.js
@@ -1,20 +1,82 @@
-import React from "react";
-import mock_data from "./assets/mock_data.json";
+import React from 'react';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
 
+import mock_data from './assets/mock_data.json';
 
-const Chart = () => {
-    return (
-        <>
-            {Object.keys(mock_data.response).map((timestamp) => (
-                <div key={timestamp}>
-                    <h3>{timestamp}</h3>
-                    <p>ID: {mock_data.response[timestamp].id}</p>
-                    <p>Value Area: {mock_data.response[timestamp].value_area}</p>
-                    <p>Value Bar: {mock_data.response[timestamp].value_bar}</p>
-                </div>
-            ))}
-        </>
-    )
-}
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController
+);
 
-export default Chart;
+export const chartLabels = Object.keys(mock_data?.response);
+export const chartDataBar = Object.values(mock_data?.response).map(entry => entry.value_bar);
+export const chartDataArea = Object.values(mock_data?.response).map(entry => entry.value_area);
+
+export const options = {
+  responsive: true,
+  scales: {
+    y1: { // 첫 번째 Y축
+      type: 'linear',
+      position: 'left',
+      id: 'y1',
+      max: 200,
+    },
+    y2: { // 두 번째 Y축
+      type: 'linear',
+      position: 'right',
+      id: 'y2',
+    },
+  },
+};
+
+export const data = {
+  labels:chartLabels,
+  datasets: [
+    {
+      type: 'line',
+      label: 'chartDataArea',
+      borderColor: 'rgb(255, 99, 132, 0.5)',
+      borderWidth: 2,
+      fill: true,
+      data: chartDataArea,
+      yAxisID: 'y1',
+    },
+    {
+      type: 'bar',
+      label: 'chartDataBar',
+      backgroundColor: 'rgb(75, 192, 192)',
+      data: chartDataBar,
+      borderColor: 'white',
+      borderWidth: 2,
+      yAxisID: 'y2',
+    },
+
+  ],
+};
+
+const userChart = () => {
+  return (
+    <Chart data={data} options={options} />
+  );
+};
+
+export default userChart;
